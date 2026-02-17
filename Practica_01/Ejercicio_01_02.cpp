7b82befb0a00a7830cd2acb74e208ae4260c6651
dianavasquez24 <diana.vasquez@ucb.edu.bo>
2026-02-17 17:29:48 -0400

    Add files via upload
    Están los ejercicios del 1 al 5

---
+++ Practica_01/Ejercicio_01_02.cpp
@@ -0,0 +1,22 @@
+//  Materia: Programación I, Paralelo 4
+//  Autor: Diana Ninoska Vasquez Benitez
+//  Carnet:8913110
+//  Carrera del estudiante:Ing. Multimedia e interactividad digital 
+//  Fecha creación: 16/02/2026
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float precio_producto , IVA= 0.13 , precio_IVA=0;
+    system ("cls");
+    
+    cout<<"Ingresar el precio del producto: "; cin >> precio_producto;
+
+    precio_IVA = precio_producto -( precio_producto*IVA );
+
+    cout <<"\nPrecio aplicando el IVA: " <<precio_IVA;
+
+    return 0;
+}
---
+++ Practica_01/Ejercicio_01_03.cpp
@@ -0,0 +1,27 @@
+//  Materia: Programación I, Paralelo 4
+//  Autor: Diana Ninoska Vasquez Benitez
+//  Carnet:8913110
+//  Carrera del estudiante:Ing. Multimedia e interactividad digital 
+//  Fecha creación: 16/02/2026
+ #include <iostream>
+
+ using namespace std;
+ int main ()
+ {
+    int edad;
+    char sexo;
+    float altura;
+    system ("cls");
+
+    cout<<"Ingresar los siguientes datos :"<< endl;
+    cout<<"Edad: ";cin >> edad;
+    cout<<"Sexo: ";cin >> sexo ;
+    cout<<"Altura: ";cin >> altura ;
+
+    cout << "\nDatos ingresados: " << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Sexo: " << sexo << endl;
+    cout << "Altura: " << altura << "\n\n";
+
+    return 0;
+ }
